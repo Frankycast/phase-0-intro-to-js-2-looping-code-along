20b305a26f8102675ace46f654013e819cd27287
Francisco Castillo <Francisco21castillo@aol.com>
2021-03-28 16:57:21 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+// Code your solutions in this fire 
+
+
+
+function writeCards(names, event){
+    let array = []
+  
+     for (let i = 0; i < names.length; i++){
+     array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+     }
+     return array
+}
+  
+function countDown(number){
+     let i = 0
+     while(i <10);{
+    console.log(i++)
+     }
+}
